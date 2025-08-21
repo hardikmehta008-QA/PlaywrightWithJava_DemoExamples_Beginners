b86eb25d3069520d23e00a5941f33b7b9d5870a1
hardikmehta008-QA <hardikmehta008@gmail.com>
2025-08-21 14:44:34 +0530

    Playwright Architecture Document

---
+++ src/main/java/PlaywrightSession/Playwright_Architecture.java
@@ -0,0 +1,40 @@
+package PlaywrightSession;
+public class Playwright_Architecture {
+    /*
+    Selenium Architecture:
+    **********************
+        Selenium works on the HTTP connection protocol. It means after you trigger the test,
+        the complete Selenium code written by us (Client) will be converted to JSON format.
+        Generated JSON is sent to the browser driver (Server) through HTTP protocol.
+
+      Selenium Client Library(Java, Python, C#)
+            <-----------> {JSON} Wire Protocol over HTTP
+                             <----------->Browser Driver(Chrome, Firefox, IE, Safari) HTTP Over HTTP Server
+                                        <----------->Browser(Chrome, Firefox, IE, Safari) HTTP Over HTTP Server
+
+     Playwright Architecture:
+     **********************
+        Playwright works on Web socket connection protocol, it means once you will trigger the test,
+        the code will be converted into JSON format and will be sent to the server using Web socket protocol.
+
+                WebSockets                  Browsers
+        TEST <--------------> Playwright <--------------> Chrome, Firefox, IE, Safari
+               All Commands
+
+The difference between Web socket connection Protocol and HTTP connection protocol:
+********************************************
+                                WebSocket Connection
+                                =====================
+Client ------------------------Request----------------------------------> Server/Cloud
+Client <------------------------HandShake-------------------------------- Server/Cloud
+Client <------------------------Web Socket------------------------------> Server/Cloud
+
+                                        VS
+                                   HTTP Connection
+                                =====================
+Client ------------------------Request----------------------------------> Server/Cloud
+Client <------------------------Response--------------------------------- Server/Cloud
+Client -------------------------Connection Terminated-------------------- Server/Cloud
+
+     */
+}
